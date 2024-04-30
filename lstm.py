a9cf96ff9ac20e02b2ef4a22e677c3ae6325fd52
henryhafner <hhafner@trinity.edu>
2024-04-30 17:47:26 -0500

    finished Q2

--- lstm.py
+++ lstm.py
@@ -11,13 +11,13 @@ from sklearn.metrics import confusion_matrix
 import seaborn as sns
 
 # Import IMDB dataset keeping only 10,000 most common unique words
-
+training_set, testing_set = imdb.load_data(num_words=10000)
 
 # Zero-Padding
-
+training_padded = sequence.pad_sequences(training_set[0], maxlen=100)
+testing_padded = sequence.pad_sequences(testing_set[0], maxlen=100)
 
 # Model Building
-
 def train_model(Optimizer, X_train, y_train, X_val, y_val):
     model = Sequential()
     # **ADD YOUR CODE HERE**
