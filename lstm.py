a4e222202c59e22585464e26d93954da12d57fb1
henryhafner <hhafner@trinity.edu>
2024-05-02 16:31:14 -0500

    changed predict to evaluate/other small changes

--- lstm.py
+++ lstm.py
@@ -23,7 +23,7 @@ training_set, testing_set = imdb.load_data(num_words=10000)
 training_padded = sequence.pad_sequences(training_set[0], maxlen=100)
 testing_padded = sequence.pad_sequences(testing_set[0], maxlen=100)
 
-hyperparameters = {'output_dim': 64, 'batch_size': 64, 'epochs': 5}    #defining hyperparameters
+hyperparameters = {'output_dim': 64, 'batch_size': 128, 'epochs': 5}    #defining hyperparameters
 
 def train_model(Optimizer, X_train, y_train, X_val, y_val):
     # Model Building
@@ -38,20 +38,22 @@ def train_model(Optimizer, X_train, y_train, X_val, y_val):
     trained_model=model.fit(X_train,y_train,batch_size=hyperparameters['batch_size'],epochs=hyperparameters['epochs'],validation_data=(X_val,y_val))
     return model, trained_model
 
-def model_prediction(model, X_val, y_val):  #makes prediction on testing data based off trained model
-    print("EVALUATING MODEL ACCURACY ON TESTING DATA")
-    make_prediction=model.predict(X_val)
-    testing_predictions=[1 if pred > 0.5 else 0 for pred in make_prediction]    #setting predictions to either 1(positive) or 0(negative)
-    correct=(testing_predictions==y_val).sum()  #counts number of predictions that were correct
-    accuracy="{:.2f}".format((correct/len(y_val))*100) #turns prediction into percentage
-    print(f"accuracy: {accuracy}%")
-    return testing_predictions
+def model_evaluation(model, X_val, y_val):  #makes evaluation on testing data based off trained model
+    print("EVALUATING MODEL ON TESTING DATA")
+    loss, accuracy = model.evaluate(testing_padded, testing_set[1])
+    accuracy="{:.2f}".format(accuracy*100) #turns accuracy into percentage
+    loss="{:.2f}".format(loss*100) #turns accuracy into percentage
+    print(f"Loss: {loss}%")
+    print(f"Accuracy: {accuracy}%")
+    print("CREATING GRAPHS")
 
-#gets base model and trained model
+
+#gets base model and trained model and prints out model evaluation
 model, trained_model=train_model(RMSprop(lr=0.1), training_padded, training_set[1], testing_padded, testing_set[1])
+model_evaluation(model, testing_padded, testing_set[1])
 
 #gets list of predictions from testing data
-testing_predictions=model_prediction(model, testing_padded, testing_set[1])
+testing_predictions=[1 if pred > 0.5 else 0 for pred in model.predict(testing_padded)]
 
 #allows us to display 2 graphs
 fig, axs = plt.subplots(2,1,figsize=(8,8))
@@ -74,5 +76,3 @@ plt.xlabel('Predicted Labels')
 plt.ylabel('Real Labels')
 plt.tight_layout()
 plt.show()
-
-
